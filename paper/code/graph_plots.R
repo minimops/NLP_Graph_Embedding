dc7813986637790ebe4d1166187a11657d3a744d
minimops <minimops.noah@gmail.com>
2021-06-25 19:26:16 +0200

    working on node2vec

--- paper/code/graph_plots.R
+++ paper/code/graph_plots.R
@@ -2,7 +2,7 @@ library(GGally)
 library(network)
 library(sna)
 library(ggplot2)
-
+library(igraph) 
 
 rmdPath <- "paper/"
 
@@ -10,9 +10,21 @@ rmdPath <- "paper/"
 ### Example Graph plot
 
 # data <- matrix(sample(0:1, 225, replace=TRUE, prob=c(0.85,0.15)), nrow=15)
-data <- rgraph(15, mode = "graph", tprob=0.2)
-net = network(data, directed = FALSE)
-ggnet2(data, label = letters[1:15])
+set.seed(2333)
+exGraph <- rbindlist(list(list(from = NULL, to = NULL, prob = NULL),
+                  list("t", "v", "1/p"),
+                  list("t", "x3", 0),
+                  list("v", "x3", 1),
+                  list("v", "x1", "1/q"),
+                  list("v", "x2", "1/q"),
+                  list("x2", "x3", 0),
+                  list("x2", "c", 0),
+                  list("x1", "c", 0),
+                  list()))
+
+net = network(exGraph, directed = FALSE)
+
+ggnet2(net, label = TRUE, edge.label = exGraph$prob)
 
 ggsave(paste0(rmdPath, "plots/exGraph.pdf"), device = "pdf")
 
