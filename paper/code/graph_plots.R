614e5f761fefde14e20078e6f8c85ca05d85a134
minimops <minimops.noah@gmailcom>
2021-08-31 22:29:54 +0200

    pre final push of paper

--- paper/code/graph_plots.R
+++ paper/code/graph_plots.R
@@ -29,3 +29,41 @@ ggnet2(net, label = TRUE, edge.label = exGraph$prob, size = 8, mode = "kamadakaw
 ggsave(paste0(rmdPath, "plots/exGraph.pdf"), device = "pdf", width = 2.9, height = 2.9)
 
 ###
+
+
+n2vgraph <- rbindlist(list(list(from = NULL, to = NULL),
+                           list("a", "b"),
+                           list("a", "c"),
+                           list("b", "c"),
+                           list("c", "d"),
+                           list("c", "e"),
+                           list("e", "f"),
+                           list("f", "g"),
+                           list("f", "h"),
+                           list("g", "i"),
+                           list("h", "i")))
+
+net = network(n2vgraph, directed = FALSE)
+
+ggnet2(net, label = TRUE, size = 8,
+       mode = "kamadakawai")
+
+ggsave(paste0(rmdPath, "plots/n2vgraph.pdf"), device = "pdf", 
+       width = 3, height = 3)
+
+###
+
+rw1 <- rbindlist(list(list(from = NULL, to = NULL),
+                           list("1", "2"),
+                           list("2", "3"),
+                           list("3", "4"),
+                           list("4", "5"))) 
+
+net = network(rw1, directed = TRUE)
+ggraph(net)
+ggnet2(net, label = TRUE, size = 8, node.label = c("f", "e", "f", "h", "i"),
+       mode = "rmds")
+
+ggsave(paste0(rmdPath, "plots/n2vgraph.pdf"), device = "pdf", 
+       width = 3, height = 3)
+                      
